e0f8f45956a7ff2b703a2d93a0b73e061235b977
paulent1y <52493425+paulent1y@users.noreply.github.com>
2019-11-06 15:08:22 +0300

    Add files via upload
    sc2 - drawing single polygon
    sc - drawing circle chasing for cursor

--- test js/script.js
+++ test js/script.js
@@ -1,9 +1,18 @@
+class Ball {
+  constructor() {
+      this.color = randomColor();
+      this.radius = Math.random() * 10 + 10;
+      this.x = Math.random() * (wx - this.radius*2);
+      this.y = Math.random() * (wy - this.radius*2);
+      this.speed = Math.random() * 10 + 2;
+  }
+}
+
 //дебажная функция
 let logPrint = () => {
     console.log(mx, my);
     setTimeout(logPrint, 10)
 }
-
 
 //задаем канвас и режим рисования
 let canvas = document.getElementById("canvas");
@@ -16,15 +25,16 @@ let wy = window.innerHeight;
 canvas.width = wx;
 canvas.height = wy;
 
-
 //читка координат мыши
-let mx = 0;
-let my = 0;
+let mx = wx/2;
+let my = wy/2;
+console.log(`Mouse X ${mx} Mouse Y ${my}`);
 addEventListener("mousemove", () => {
     mx = event.clientX;
     my = event.clientY;
+    // c.clearRect(0,0,wx,wy)
+    // drawCircles();
 })
-
 
 //выбор случайного цвета
 function randomColor() {
@@ -39,47 +49,101 @@ function randomColor() {
       Math.ceil(Math.random() * 10) / 10 +
       ")"
     );
+}
+
+//рисовалка примитивов 
+function drawCircles(amount) { 
+  for (let i = 0; i< amount; i++) {
+      c.beginPath(); //начало пути рисования
+      c.fillStyle = randomColor(); // выбор цвета
+      //дуга (х,у, r, начальный градус, конечный градус (в радианах что ли))
+      c.arc(Math.random() * wx,
+              Math.random() * wy,
+              20 + Math.random() * 60,
+              0,
+              2 * Math.PI);
+      c.fill();  //зарисовка внутри
+      //c.stroke(); // зарисовка линией по контуру
   }
+}
+
+function drawRectangles(amount) { 
+  for (let i = 0; i< amount; i++) {  
+      // рисовалка прямоугольников
+      c.fillStyle = randomColor();
+      a = Math.random() * (0.5*wx);
+      b = Math.random() * (0.5*wy);
+      c.fillRect(a,b,a + Math.random() * 30,b + Math.random() * 30);
+  }
+}  
+
+let character = new Ball;
+console.log(character);
+
+function drawHero() {
+  c.beginPath(); 
+  // c.fillStyle = character.color;
+  c.fillStyle = '#FF0000' 
+  c.arc(character.x,
+          character.y,
+          character.radius,
+          0,
+          2 * Math.PI);
+  c.fill();  
+}
+
+function moveHero() {
+  character.x += (mx-character.x) /character.speed;
+  character.y += (my-character.y) /character.speed;
 
 
-//рисовалка примитивов  
-for (let i = 0; i< 1000; i++) {
-    
-    /* рисовалка прямоугольников
-    c.fillRect(Math.random() * wx,
-                Math.random() * wy,
-                Math.random() * wx + Math.random() * 50, 
-                Math.random() * wy + Math.random() * 50);
-    */
-
-
-
-   //рисовалка кругов
-    c.beginPath(); //начало пути рисования
-    c.fillStyle = randomColor(); // выбор цвета
-    //дуга (х,у, r, начальный градус, конечный градус (в радианах что ли))
-    c.arc(Math.random() * wx,
-            Math.random() * wy,
-            20 + Math.random() * 60,
-            0,
-            2 * Math.PI);
-
-    c.fill();  //зарисовка внутри
-    //c.stroke(); // зарисовка линией по контуру
+  // if (character.y > my)
+  //   character.y--;
+  // else if (character.y < my)
+  //   character.y++
+  // else {}
 
 }
 
 
+updateScreen();
 
-// class Ball {
-//     constructor() {
-//         this.color = randomColor();
-//         this.radius = Math.random() * 20;
-//         this.x = Math.random() * (wx - this.radius*2);
-//         this.y = Math.random() * (wy - this.radius*2);
+function updateScreen () {
+  c.clearRect(0,0,wx,wy);
+  // drawCircles(20);
+  // drawRectangles(20);
+  drawHero();
+  moveHero();
+  initialDelay = 33;
+  setTimeout(updateScreen, initialDelay); //17ms for 60 fps, 33ms for 30fps
+}
 
-//     }
-// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // let balls = [];
 // for (let i =0; i<20; i++) {
---
+++ test js/script2.js
@@ -0,0 +1,33 @@
+// Многомерный массив с координатами
+var coordArray = [
+    [130,195],
+    [19,158],
+    [19,41],
+    [130,5],
+    [200,100]
+    ];
+
+let canvas = document.getElementById("canvas");
+let c = canvas.getContext("2d");
+console.log(c)
+
+//задаем размеры окна и канваса
+let wx = window.innerWidth;
+let wy = window.innerHeight;
+canvas.width = wx;
+canvas.height = wy;
+
+// Функция, рисующая полигон из массива координат
+function drawPoly(arr) {
+    var canvas = c;
+    canvas.beginPath(); // Начинаем отрисовку
+    for (var i = 0; i < arr.length; i++) {
+        if (i == 0) canvas.moveTo(arr[i][0], arr[i][1]); // Ставим точку на исходную позицию
+        else canvas.lineTo(arr[i][0], arr[i][1]); // Рисуем линии от точки к точке
+    }
+    canvas.fillStyle = "rgba(255,128,128,0.5)"; // Задаем цвет заливки в формате RGBA
+    canvas.fill(); // Зальем полученный многоугольник цветом
+    }
+    
+    
+drawPoly(coordArray);
