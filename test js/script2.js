a75390242ff89166e6a0af1c00f0c2143fdb14ba
Paul <gispavel2017@gmail.com>
2019-11-08 02:08:39 +0300

    sc3 added

--- test js/script2.js
+++ test js/script2.js
@@ -1,21 +1,24 @@
 // Многомерный массив с координатами
-var coordArray = [
-    [130,195],
+let coordArray = [[130,195],
     [19,158],
     [19,41],
     [130,5],
     [200,100]
     ];
 
+document.getElementsByTagName('body')[0].innerHTML = `<div class="slidecontainer">
+<input type="range" min="3" max="5000" value="5" class="slider" id="myRange" style = "width: 100%">
+</div>` + document.getElementsByTagName('body')[0].innerHTML;
 let canvas = document.getElementById("canvas");
 let c = canvas.getContext("2d");
-console.log(c)
-
 //задаем размеры окна и канваса
 let wx = window.innerWidth;
 let wy = window.innerHeight;
 canvas.width = wx;
 canvas.height = wy;
+let radius = (wx<wy) ? wx/3 : wy/3 ;
+let offsetx = wx/2;
+let offsety = wy/2;
 
 // Функция, рисующая полигон из массива координат
 function drawPoly(arr) {
@@ -28,10 +31,38 @@ function drawPoly(arr) {
     canvas.fillStyle = "rgba(255,128,128,0.5)"; // Задаем цвет заливки в формате RGBA
     canvas.fill(); // Зальем полученный многоугольник цветом
     }
+
     
-function createArray() {
-    
+
+function createPolyArray(n, arr) {
+    let angle = 180 - (((n-2)/n) * 180)
+    let currAngle = 0;
+    for (let i = 0; i<n; i++)
+    {   
+        let xycoords = []
+        currAngle+= angle;
+        radAngle = currAngle / (180/Math.PI);
+        xycoords.push(offsetx + Math.ceil(radius * Math.cos(radAngle)));
+        xycoords.push(offsety + Math.ceil(radius * Math.sin(radAngle)));
+        arr.push(xycoords);
+    }
+    console.log(arr);
+    console.log(  `${Math.ceil((1 - (( (180*(n-2)) /n)  /  (180*(n-2)))) * 10000)/100} % закругленности`  );
 }
-    
-drawPoly(coordArray);
+
+
+
+let amount_of_angles = 5
+let slider = document.getElementById('myRange');
+slider.oninput = function() {
+    let anotherArray = [];
+    amount_of_angles = this.value;
+    c.clearRect(0,0,wx,wy);
+    createPolyArray(amount_of_angles,anotherArray)
+    drawPoly(anotherArray);
+} 
+
+let anotherArray = [];
+createPolyArray(amount_of_angles,anotherArray)
+drawPoly(anotherArray);
 
---
+++ test js/script3.js
@@ -0,0 +1,104 @@
+//пробуем нарисовать квадырат
+
+const FRAME_DELAY = 17; //17 for 60 fps, 33 for 30 fps
+const FIELD_ARRAY = []; // массив, хранящий принадлежность ячеек
+const TILES_AMOUNT = 20; // количество ячеек на поле (по каждой из координат)
+const FIELD_COLOR = randomColor();
+const TILE_COLOR_N = randomColor();
+const COLOR_P1 = 0;
+const COLOR_P2 = 0;
+
+for (let i =0; i<TILES_AMOUNT;i++){
+    FIELD_ARRAY.push([]);
+    for (let j = 0; j<TILES_AMOUNT; j++)
+        FIELD_ARRAY[i].push(0);
+    //заполнение массива игровых клеток нулями
+}
+
+
+console.log(FIELD_ARRAY);
+
+//стандартные настроечки размеров канваса
+let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext('2d');
+let wx = window.innerWidth;
+let wy = window.innerHeight;
+
+canvas.width = wx;
+canvas.height = wy;
+
+function screenUpdate() {
+    console.log(1);
+    //эта функция вызывается в конце файла через SetInterval
+    //задумана для обновления содержимого экрана
+}
+
+function drawGameField() {
+    let xpos = wx/2;
+    let ypos = wy/2;
+    //ищем центр экрана(канваса)
+    let fieldSide;
+
+    //задаем сторону игрового поля по меньшей стороне
+    if (wx<wy)
+        fieldSide = Math.ceil(wx*0.8);
+    else 
+        fieldSide = Math.ceil(wy*0.8);
+   
+    //находим координаты x и y угловых точек игрового поля
+    let fs = Math.ceil(fieldSide/2);
+    let ax = Math.ceil(xpos-fs);
+    let ay = Math.ceil(ypos-fs);
+    let bx = Math.ceil(xpos+fs);
+    let by = Math.ceil(ypos+fs);
+    
+
+    //начинаем размеренно и тупо рисовать поляну 
+    //fillRect отказался работать, надо бы проверить его где то еще раз
+    ctx.beginPath();
+    ctx.lineWidth = 0.5;
+    ctx.moveTo(ax,ay);
+    ctx.lineTo(bx,ay);
+    ctx.lineTo(bx,by);
+    ctx.lineTo(ax,by);
+    ctx.lineTo(ax,ay);
+    ctx.fillStyle = FIELD_COLOR;
+    ctx.fill()
+    ctx.stroke();
+
+    /*
+    считаем размер одной игровой клетки 
+    (делим поле на количество клеток и берем 95 процентов, 
+    чтобы сделать отступ между ними)
+    */
+    let tileSize = Math.ceil((fieldSide / TILES_AMOUNT) * 95)/100;
+
+    //вывод в консольку для отладки зачем я это пишу
+    console.log(ax,ay,fs*2,fs*2);
+    console.log(TILES_AMOUNT, tileSize, fieldSide);
+}
+
+
+/*
+рандомные цвета (вместо 250 поставил 150 для более мягких цветов)
+можно попробовать добавить к цвету оффсет, 
+чтобы не брались нижние пороги цветов
+*/
+function randomColor() { 
+    return (
+      "rgba(" +
+      Math.round(Math.random() * 150) +
+      "," +
+      Math.round(Math.random() * 150) +
+      "," +
+      Math.round(Math.random() * 150) +
+      "," +
+      Math.ceil(Math.random() * 10) / 20 +
+      ")"
+    );
+}
+
+
+
+drawGameField(); // рисуем поле 
+setInterval(screenUpdate, FRAME_DELAY); //задаем обновляшку экрана передавая фреймрейт
